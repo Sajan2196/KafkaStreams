b16dacce9d500492e45abd03feabe4dd8ed54920
jetpu <jetpuriya96@gmail.com>
2025-01-17 11:38:32 +0530

    [17/01/25]learnkafkastreaming

---
+++ src/main/java/com/learnkafkastreaming/greeting/streams/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.learnkafkastreaming.greeting.streams.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafkaStreams
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/learnkafkastreaming/greeting/streams/springboot/Configuration/GreetingStreamsConfiguration.java
@@ -0,0 +1,33 @@
+package com.learnkafkastreaming.greeting.streams.springboot.Configuration;
+
+import com.learnkafkastreaming.greeting.streams.springboot.topology.GreetingStreamsTopology;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class GreetingStreamsConfiguration {
+
+    @Bean
+    public StreamsBuilder streamsBuilder() {
+        return new StreamsBuilder();
+    }
+
+    @Bean
+    public NewTopic greetingsTopic(){
+        return TopicBuilder.name(GreetingStreamsTopology.GREETINGS)
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic greetingsOutputTopic(){
+        return TopicBuilder.name(GreetingStreamsTopology.GREETINGS_OUTPUT)
+                .partitions(2)
+                .replicas(1)
+                .build();
+    }
+}
